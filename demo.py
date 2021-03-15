d727e50a764f947bcd745e298f5d35e01d4396ab
cavilosa <cavilosa33@gmail.com>
2021-03-15 13:38:17 -0600

    Created psycopg2 app file in python for the db(example)

--- demo.py
+++ demo.py
@@ -1,4 +1,22 @@
 import psycopg2
 
 
-conn = psycopg2.connect('dbname=example')
+conn = psycopg2.connect('dbname=example user=postgres password=cavilosa1')
+
+
+cursor = conn.cursor()
+
+cursor.execute('drop table if exists table4;')
+
+cursor.execute('''
+    create table table4 (
+        id integer primary key,
+        completed boolean not null default false
+    );
+''')
+
+cursor.execute('insert into table4 values (1, false), (2, true);')
+
+conn.commit()
+conn.close()
+cursor.close()
